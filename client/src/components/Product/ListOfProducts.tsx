a002539649fb7dc41605c82e2a229d1064c35234
Helga998 <bom1993@yandex.ru>
2024-06-20 14:23:42 +0300

    resposive navbar

---
+++ client/src/components/Product/ListOfProducts.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ListOfProducts(props) {
+  return (
+    <div>
+      Products
+    </div>
+  );
+}
+
+export default ListOfProducts;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import App from './App/App';
 import './index.css';
-import { Provider } from 'react-redux';
+
 import { store } from './App/redux/store';
+import Nav from './components/Navbar/Nav';
+import ErrorPage from './components/Navbar/ErrorPage';
+import ListOfProduct from './components/Product/ListOfProducts';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Nav />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/products',
+        element: <ListOfProduct />,
+      },
+      // {
+      //   path: 'auth',
+      //   element: <LogIn />,
+      
+      // },
+      // {
+      //   path: 'reg',
+      //   element: <Reg />,
+      
+      // },
+    
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
-    <App />
+     <RouterProvider router={router} />
     </Provider>,
 );
