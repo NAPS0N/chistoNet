be6d2f34596f80fe3eb9e6747ee0f24d273252fe
Molibog <mikhail.m02@ya.ru>
2024-06-21 14:30:37 +0300

    fix: database chat

--- servers/db/models/chat.js
+++ servers/db/models/chat.js
@@ -1,25 +1,21 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Chat extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      this.belongsTo(models.Chat, { foreignKey: 'userId' });
-
+      this.belongsTo(models.Chat, { foreignKey: "fromId" });
+      this.belongsTo(models.Chat, { foreignKey: "toId" });
     }
   }
-  Chat.init({
-    message: DataTypes.TEXT,
-    userId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Chat',
-  });
+  Chat.init(
+    {
+      message: DataTypes.TEXT,
+      userId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Chat",
+    }
+  );
   return Chat;
 };
--- servers/db/seeders/20240620111754-Chats.js
+++ servers/db/seeders/20240620111754-Chats.js
@@ -1,25 +1,55 @@
-'use strict';
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Chats",
+      [
+        {
+          id: 1,
+          message: "Привет UserId 2",
+          fromId: 1,
+          toId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          message: "Привет UserId 1",
+          fromId: 2,
+          toId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 3,
+          message: "Как дела UserId 2?",
+          fromId: 1,
+          toId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 4,
+          message: "У меня всё хорошо. Как твои дела userId 1?",
+          fromId: 2,
+          toId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 5,
+          message: "Да по-тихой UserId 2. Только откинулся))",
+          fromId: 1,
+          toId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Chat", null, {});
+  },
 };
