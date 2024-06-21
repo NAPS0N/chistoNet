1589df9e90e77c7250b8003debafb96f4dba29bc
NAPS0N <vs-bragin@yandex.ru>
2024-06-21 11:59:33 +0300

    Auth_Done

--- client/src/components/Auth/Registration.tsx
+++ client/src/components/Auth/Registration.tsx
@@ -20,24 +20,38 @@ function Registration(): JSX.Element {
   const navigate = useNavigate();
   const err = useAppSelector((state) => state.auth.error);
 
-//   const logIn = async (): Promise<void> => {
-//     const data = await dispatch(registrationThunk(signUpForm));
+  const logIn = async (): Promise<void> => {
+    const data = await dispatch(registrationThunk(signUpForm));
 
-//     if (data.payload) {
-//       dispatch(showAlert({ message: "Welcome", type: 'success' }));
-//       navigate('/');
-//     } else {
-//       dispatch(showAlert({ message: err || 'Error =)', type: 'error' }));
-//       dispatch(clearError());
-//     }
-//   };
+    // if (data.payload) {
+    //   dispatch(showAlert({ message: "Welcome", type: 'success' }));
+    //   navigate('/');
+    // } else {
+    //   dispatch(showAlert({ message: err || 'Error =)', type: 'error' }));
+    //   dispatch(clearError());
+    // }
+  };
 
   return (
     <form
       style={{ width: '360px', padding: '8% 0 0', margin: 'auto' }}
       onSubmit={(e) => e.preventDefault()}
     >
-      <label htmlFor="login" className="form-label">
+
+<label htmlFor="email" className="form-label">
+        Email
+      </label>
+      <input
+        type="text"
+        placeholder="Email"
+        value={signUpForm.email}
+        onChange={(e) => setSignUpForm({ ...signUpForm, email: e.target.value })}
+        className="form-control"
+        id="email"
+      />
+
+
+      {/* <label htmlFor="login" className="form-label">
         Login
       </label>
       <input
@@ -47,7 +61,8 @@ function Registration(): JSX.Element {
         onChange={(e) => setSignUpForm({ ...signUpForm, login: e.target.value })}
         className="form-control"
         id="login"
-      />
+      /> */}
+
       <label htmlFor="password" className="form-label">
         Password
       </label>
@@ -60,29 +75,8 @@ function Registration(): JSX.Element {
         id="password"
       />
 
-      <label htmlFor="email" className="form-label">
-        Email
-      </label>
-      <input
-        type="text"
-        placeholder="Email"
-        value={signUpForm.email}
-        onChange={(e) => setSignUpForm({ ...signUpForm, email: e.target.value })}
-        className="form-control"
-        id="email"
-      />
+      
 
-      <label htmlFor="age" className="form-label">
-        Age
-      </label>
-      <input
-        type="number"
-        placeholder="Age"
-        value={signUpForm.age}
-        onChange={(e) => setSignUpForm({ ...signUpForm, age: e.target.value })}
-        className="form-control"
-        id="age"
-      />
       <div className="d-grid gap-1">
         <button type="submit" onClick={() => void logIn()} className="btn btn-success">
           Зарегистрироваться
