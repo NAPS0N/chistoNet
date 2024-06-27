18467accb1814f56ecf5e0ed39d8dc65fd3866d1
Helga998 <bom1993@yandex.ru>
2024-06-27 18:48:38 +0300

    Img added

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ProductItem from '../Product/ProductItem';
 import { useAppSelector } from '../../App/redux/store';
+import './Home.css';
 
 function Home(): JSX.Element {
   const  products = useAppSelector((store) => store.products.products);
+  const news = useAppSelector((store) => store.news.news)
+  console.log(news, 'news');
+  
 
   return (
     <>
-    <div className='home-image'>
+    <div className="home-image-container">
       <img src="../../../public/homePage.png" alt="" className='home-image'/>
     </div>
     <h2>Товары</h2>
